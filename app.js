5267d01d3bf669093d69fcd4d1ea64d5bacbb7c0
Moksh parashar <parasharm239@gmail.com>
2024-03-31 13:07:46 +0530

    .

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import cors from "cors";
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 app.use(
   cors({
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,32 @@
 import { User } from "../models/user.model.js";
 
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
-    const existedUser = await User.findOne(email);
+    const existedUser = await User.findOne({ email });
     if (existedUser) {
-      throw new Error("credentials already exists");
+      res.send({
+        message: "Credentials already exists",
+        code: 403,
+        ok: false,
+      });
+      throw new Error(400, "credentials already exists");
     }
     await User.create({
       name,
       email,
       password,
     });
+
+    res.send({
+      message: "user creation successful",
+      code: 200,
+      ok: true,
+    });
   } catch (error) {
     console.log(error);
   }
+  res.end();
+  next();
 };
