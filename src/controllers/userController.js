3d64483e963a4c49da8777e73f6e0cc4bca1e98a
Moksh parashar <parasharm239@gmail.com>
2024-03-31 09:08:26 -0700

    register+login routes work done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,7 +21,7 @@ export const registerUser = async (req, res, next) => {
 
     res.send({
       message: "user creation successful",
-      user: {
+      userData: {
         name,
         email,
       },
@@ -31,6 +31,44 @@ export const registerUser = async (req, res, next) => {
   } catch (error) {
     console.log(error);
   }
+  res.end();
+  next();
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      res.send({
+        message: "User not found",
+        code: 404,
+        ok: false,
+      });
+      throw new Error(404, " Email is not registered");
+    } else {
+      if (password === user.password) {
+        res.send({
+          message: "Congratulations you are in",
+          userData: { name: user.name, email: user.email },
+          code: 200,
+          ok: true,
+        });
+      } else {
+        res.send({
+          message: "Invalid credentials",
+          code: 400,
+          ok: false,
+        });
+        throw new Error(400, "invalid credentials");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
   res.end();
   next();
 };
