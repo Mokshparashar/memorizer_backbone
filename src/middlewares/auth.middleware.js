bb6dbf2fbea799ea54afe73e797ccc5173ccbd1b
Moksh parashar <parasharm239@gmail.com>
2024-04-19 05:58:49 -0700

    .

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -11,7 +11,7 @@ export const verifyJwt = asyncHandler(async (req, res, next) => {
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedToken?._id).select(
       "-password",
-      "refreshToken"
+      "-refreshToken"
     );
 
     if (!user) {
